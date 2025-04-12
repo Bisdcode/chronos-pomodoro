0662594f0d989725f8430bf67cd57351087adc00
Bisdcode <bismarck.sdcode@gmail.com>
2025-04-12 17:56:59 -0300

    create firs component

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
+    console.log("OI")
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+    return (
+    <div>
+        <h1>Olá Mundo! (do App)</h1>
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+            Este é um exemplo de aplicação React com TypeScript e Vite.
+            <br />
+            <code>src/App.tsx</code>
         </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+    );
 }
 
-export default App
+export default App;
