2a8d8c25fc02619fc91b1def70f08f5628a42b97
Bisdcode <bismarck.sdcode@gmail.com>
2025-04-13 07:33:09 -0300

    heading config and type

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,25 @@
-import { Heading } from './components/Heading';
+import { Heading } from "./components/Heading";
 
-import './styles/theme.css';
-import './styles/global.css';
+import "./styles/theme.css";
+import "./styles/global.css";
+import { TimerIcon } from "lucide-react";
 
 function App() {
-    console.log("Oi")
-
-    return (
-    <div>
-        <Heading attr={123} attr2='String'>Olá mundo!</Heading>
-        <p>
-            Este é um exemplo de aplicação React com TypeScript e Vite.
-            <br />
-            <code>src/App.tsx</code>
-        </p>
-    </div>
-    );
+	return (
+		<div>
+			<Heading>
+				Olá mundo!
+				<button>
+					<TimerIcon />
+				</button>
+			</Heading>
+			<p>
+				Este é um exemplo de aplicação React com TypeScript e Vite.
+				<br />
+				<code>src/App.tsx</code>
+			</p>
+		</div>
+	);
 }
 
 export default App;
