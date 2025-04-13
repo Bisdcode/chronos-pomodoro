c384e340788339fb19006828b6e365d2f649c718
Bisdcode <bismarck.sdcode@gmail.com>
2025-04-13 10:12:37 -0300

    html and css app

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,37 @@
-import { Heading } from "./components/Heading";
-
 import "./styles/theme.css";
 import "./styles/global.css";
-import { TimerIcon } from "lucide-react";
 
 function App() {
 	return (
-		<div>
-			<Heading>
-				Olá mundo!
-				<button>
-					<TimerIcon />
-				</button>
-			</Heading>
-			<p>
-				Este é um exemplo de aplicação React com TypeScript e Vite.
-				<br />
-				<code>src/App.tsx</code>
-			</p>
-		</div>
+		<>
+			<div className="container">
+				<div className="content">
+					<section>LOGO...</section>
+				</div>
+			</div>
+
+			<div className="container-fluid">
+				<div className="container">
+					<div className="content">
+						<section>Menu...</section>
+					</div>
+				</div>
+			</div>
+			<div className="container-fluid">
+				<div className="container">
+					<div className="content">
+						<section>FORM...</section>
+					</div>
+				</div>
+			</div>
+			<div className="container-fluid">
+				<div className="container">
+					<div className="content">
+						<section>Footer...</section>
+					</div>
+				</div>
+			</div>
+		</>
 	);
 }
 
