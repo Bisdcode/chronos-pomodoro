b241aafd89562b119c1ca47bf574af8217299cbe
Bisdcode <bismarck.sdcode@gmail.com>
2025-06-15 15:24:44 -0300

    create home and template

---
+++ src/pages/NoteFound/index.tsx
@@ -0,0 +1,14 @@
+import { Container } from "../../components/Container";
+import { MainTemplate } from "../../templates/MainTemplate";
+
+export function NoteFound() {
+    return (
+        <MainTemplate>
+            <Container>
+                <title>Página não encontrada :/!</title>
+                <h1>Página não encontrada!</h1>
+            </Container>
+        </MainTemplate>
+
+    )
+}
