8ee11383e89f44fe2ad73702fe139d06ac223490
Bisdcode <bismarck.sdcode@gmail.com>
2025-06-15 15:47:48 -0300

    genericHtml component

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import './styles/global.css'
 
 import { Home } from './pages/Home'
 import { NoteFound } from './pages/NoteFound'
+import { AboutPomodoro } from './pages/AboutPomodoro'
 
 export function App() {
   return <Home />
