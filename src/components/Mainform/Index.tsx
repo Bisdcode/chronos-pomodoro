c2ad9f76abaf5a95e03aa7e72192140722f88d21
Bisdcode <bismarck.sdcode@gmail.com>
2025-06-28 11:04:41 -0300

    newTask MainForm

--- src/components/Mainform/Index.tsx
+++ src/components/Mainform/Index.tsx
@@ -2,15 +2,52 @@ import { PlayCircleIcon } from "lucide-react";
 import { Cycles } from "../Cycles";
 import { DefaultButton } from "../DefaultButton";
 import { DefaultInput } from "../DefaultInput";
-import { useRef, useState } from "react";
+import { useRef } from "react";
+import { TaskModel } from "../../models/TaskModel";
+import { useTaskContext } from "../../contexts/TaskContext/useTaskContext";
 
 export function MainForm() {
+  const { setState } = useTaskContext();
   const taskNameInput = useRef<HTMLInputElement>(null)
 
   function handleCreateNewTask(e: React.FormEvent<HTMLFormElement>) {
     e.preventDefault();
 
-    console.log('Deu certo', taskNameInput.current?.value)
+    if (taskNameInput.current === null) return;
+
+    const taskName = taskNameInput.current.value.trim();
+
+
+    if (!taskName) {
+      alert("Digite uma tarefa valida")
+      return;
+    }
+
+    const newTask: TaskModel = {
+      id: Date.now().toString(),
+      name: taskName,
+      startDate: Date.now(),
+      completeDate: null,
+      interruptDate: null,
+      duration: 1,
+      type: "workTime",
+    }
+
+    // console.log(newTask)
+    const secondsRemaining = newTask.duration * 60;
+
+    setState((prevState) => {
+      return {
+        ...prevState,
+        config: { ...prevState.config },
+        activeTask: newTask, // A conferir depois
+        currentCycle: 1, // A conferir depois
+        secondsRemaining: secondsRemaining,
+        formattedSecondsRemaining: '00:60', // A conferir depois
+        tasks: [...prevState.tasks, newTask],
+      }
+    })
+
   }
 
   return (
